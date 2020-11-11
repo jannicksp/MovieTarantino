492eb455ac082013c263c8adca69a457aa4a53d8
Annalee Engsted <annalee.engsted@hotmail.com>
2020-11-11 15:29:46 +0100

    modal works now

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 "use strict";
+//empty array to store JSON data
 let movieData= [];
+//Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 window.addEventListener("DOMContentLoaded", init);
 
 function init(){
@@ -16,8 +24,8 @@ function loadJSON() {
     });
 
 }
-
-// fetch("path", {
+//for later API fetch:
+// fetch("pathToFetchURL", {
 //   method: "get",
 //   headers: {
 //     "Content-Type": "application/json; charset=utf-8",
@@ -28,50 +36,48 @@ function loadJSON() {
 
 
 function showData(){
-  console.log("showData");
   //set destination for where all the clones should go
   const dest = document.querySelector("section#moviesList");
   //get the template for the movie
   const tempMovie = document.querySelector("template");
 
+   //For each movie object in the JSON file, 1. make a clone from the template, 2. add it to the DOM, 3. add an eventlistener
   movieData.forEach(movie => {
+    //1:
     let clone = tempMovie.cloneNode(true).content;
-
     clone.querySelector("img").src = movie.img;
     clone.querySelector("img").alt = "placeholder alt";
     // clone.querySelector("h3").innerHTML = movie.name;
   
-   
+  
+   //2:
     dest.appendChild(clone);
-  }); //forEach loop end
 
-  document.querySelectorAll(".movie").forEach(movie =>{
-    movie.addEventListener("click", openModal);
-  })
+    //3:
+    dest.lastElementChild.addEventListener("click", ()=>{
+      modal.style.display = "block";
+document.querySelector("#myModal h2").innerHTML = movie.name;
+document.querySelector("#myModal p").innerHTML = movie.description;
+document.querySelector(".modal-content").lastElementChild.innerHTML = movie.year;
+  //to do: add the rest, video src etc
+    })
+  }); 
+  //forEach loop end. 
+ 
+
+//in case of bigger scope, otherwise just delete:
+  // document.querySelectorAll(".movie").forEach(movie =>{
+  //   movie.addEventListener("click", openModal);
+  // })
+
+ 
 
 }
 
-
-
-
-
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
 // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+span.onclick = () => {
+  modal.style.display = "none";
+}
 
 //When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -81,9 +87,3 @@ window.onclick = function(event) {
 }
 
 
-function openModal(event){
-  // Get the modal
-//get the id, show data for that id here.....
-  modal.style.display = "block";
-  
-}
